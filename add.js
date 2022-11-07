0739c7194d57b9e8706d51205cb86e6a3607072c
maharadgy-242 <116069313+maharadgy-242@users.noreply.github.com>
2022-11-07 19:40:40 +0100

    add console.log

--- add.js
+++ add.js
@@ -67,6 +67,7 @@ function getDaysForecast(coordinates) {
 }
 
 function displayTemperature(response) {
+  console.log(response.data);
   let temperatureElement = document.querySelector("#temperature");
   temperatureElement.innerHTML = Math.round(response.data.temperature.current);
 
